a1d6c3db59b5d5a602abc3e352844429fe40e9e0
Ernesto <93765806+ErnestoFernandezUA@users.noreply.github.com>
2022-06-25 20:50:48 +0300

    ver1

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,103 @@
 import React from 'react';
 import './App.scss';
+import { NewUserForm } from './NewUserForm';
+import { UsersList } from './UsersList';
 
-interface Props {
-  onClick: () => void;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
-
-export const App: React.FC = () => {
-  return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
-    </div>
-  );
+type User = {
+  id: number;
+  name: string;
 };
+
+type State = {
+  users: User[];
+};
+
+const usersFromServer = [
+  { id: 1, name: 'Anna' },
+  { id: 2, name: 'Laila' },
+  { id: 3, name: 'Lena' },
+];
+
+export class App extends React.Component<{}, State> {
+  state = {
+    users: usersFromServer,
+  };
+
+  // addStudent = () => {
+  //   this.setState((prevState) => ({ studentsCount: prevState.studentsCount + 1 }));
+  // };
+
+  // removeStudent = () => {
+  //   this.setState((prevState) => ({ studentsCount: prevState.studentsCount - 1 }));
+  // };
+
+  // addEmployee = () => {
+  //   this.setState((prevState) => ({ employeesCount: prevState.employeesCount + 1 }));
+  // };
+
+  // removeEmployee = () => {
+  //   this.setState((prevState) => ({ employeesCount: prevState.employeesCount - 1 }));
+  // };
+
+  addUser = (name: string) => {
+    this.setState(prevState => {
+      const newUser = {
+        id: Math.max(...prevState.users.map(user => user.id)) + 1,
+        name,
+      };
+
+      return {
+        users: [...prevState.users, newUser],
+      };
+    });
+
+    // eslint-disable-next-line no-console
+    console.log(this.state.users);
+  };
+
+  deleteUser = (userId: number) => {
+    this.setState(prevState => ({
+      users: prevState.users.filter(
+        user => user.id !== userId,
+      ),
+    }));
+  };
+
+  renameUser = (userId: number, name: string) => {
+    this.setState(prevState => ({
+      users: prevState.users.map(user => {
+        if (user.id === userId) {
+          return {
+            ...user,
+            name,
+          };
+        }
+
+        return user;
+      }),
+    }));
+  };
+
+  render() {
+    const {
+      users,
+    } = this.state;
+
+    // eslint-disable-next-line no-console
+    console.log(users);
+
+    return (
+      <div className="">
+        <NewUserForm
+          onAdd={this.addUser}
+        />
+
+        <UsersList
+          users={users}
+          onDelete={this.deleteUser}
+          onRename={this.renameUser}
+        />
+      </div>
+    );
+  }
+}
